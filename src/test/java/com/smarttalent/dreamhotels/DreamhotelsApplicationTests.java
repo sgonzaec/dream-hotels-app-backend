823f24cfc2dc234d36c7ae69922a5562c613cd86
Simon Gonzalez Echavarria <simongonzalezechavarria@gmail.com>
2023-07-15 19:17:01 -0500

    initial commit

---
+++ src/test/java/com/smarttalent/dreamhotels/DreamhotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttalent.dreamhotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamhotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
