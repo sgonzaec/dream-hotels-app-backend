483e6a721ea75ff6fa64d5d9ce02176fd40d4673
Simon Gonzalez Echavarria <simongonzalezechavarria@gmail.com>
2023-07-15 19:35:03 -0500

    update base app

--- src/main/java/com/smarttalent/dreamhotels/DreamhotelsApplication.java
+++ src/main/java/com/smarttalent/dreamhotels/DreamhotelsApplication.java
@@ -12,7 +12,7 @@ public class DreamhotelsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DreamhotelsApplication.class, args);
 	}
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
