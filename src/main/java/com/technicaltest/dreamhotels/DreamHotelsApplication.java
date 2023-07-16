0ce9a3fbe03c9a515746ffe2e487f28fad332ed7
Simon Gonzalez Echavarria <simongonzalezechavarria@gmail.com>
2023-07-16 17:42:05 -0500

    feat: add swagger

--- src/main/java/com/technicaltest/dreamhotels/DreamHotelsApplication.java
+++ src/main/java/com/technicaltest/dreamhotels/DreamHotelsApplication.java
@@ -3,10 +3,9 @@ package com.technicaltest.dreamhotels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
+
 @ComponentScan(basePackages = "com.technicaltest.dreamhotels")
 public class DreamHotelsApplication {
 	public static void main(String[] args) {
